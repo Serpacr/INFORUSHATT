7c8455a0324d78323376b3339d2593b4d3506fad
Serpacr <164097767+Serpacr@users.noreply.github.com>
2025-06-04 19:02:55 -0300

    Update ConteudoRepository.java

--- InfoRush-main/InfoRush-main/InfoRush_Final/InfoRush/src/main/java/com/inforush/repository/ConteudoRepository.java
+++ InfoRush-main/InfoRush-main/InfoRush_Final/InfoRush/src/main/java/com/inforush/repository/ConteudoRepository.java
@@ -1,9 +1,13 @@
 package com.inforush.repository;
 
-import com.inforush.model.*;
+import com.inforush.model.Conteudo;
+import com.inforush.model.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface ConteudoRepository extends JpaRepository<Conteudo, Long> {
     List<Conteudo> findByArea(Area area);
     List<Conteudo> findByTituloContainingIgnoreCase(String titulo);
