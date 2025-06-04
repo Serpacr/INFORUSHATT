0fddabbcd2c1f4b0ea6870b55c03abf22465830f
Serpacr <164097767+Serpacr@users.noreply.github.com>
2025-06-04 19:04:24 -0300

    Update Conteudo.java

--- InfoRush-main/InfoRush-main/InfoRush_Final/InfoRush/src/main/java/com/inforush/model/Conteudo.java
+++ InfoRush-main/InfoRush-main/InfoRush_Final/InfoRush/src/main/java/com/inforush/model/Conteudo.java
@@ -1,31 +1,28 @@
 package com.inforush.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
+@Table(name = "conteudos")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Conteudo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotBlank
+    private String titulo;
+
+    @Lob
+    private String descricao;
+
     @Enumerated(EnumType.STRING)
     private Area area;
 
-    @NotBlank
-    @Size(max = 100)
-    private String titulo;
-
-    @NotBlank
-    @Size(min = 10, max = 5000)
-    private String descricao;
-
-    @ManyToOne
-    @JoinColumn(name = "autor_id")
-    private Usuario autor;
+    // outros campos que julgar necessários
 }
